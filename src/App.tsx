54a4908d20e19483a8401b9f7752f25f0144def0
Mena Wang <wang.mena@gmail.com>
2020-01-03 22:11:25 -0800

    Added react and test layout component

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Layout } from './Layout';
+
+ReactDOM.render(<Layout />, document.getElementById('root'));
---
+++ src/Layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export interface LayoutProps {
+
+}
+
+export const Layout: React.FC<LayoutProps> = (props: LayoutProps) => {
+    return (
+        <div>
+            <p> test </p>
+        </div>
+    );
+}
