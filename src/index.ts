bb32ada78d41bbf432063381a78360a2bb97c862
Kenneth Fang <kwf37@cornell.edu>
2020-01-03 19:22:03 -0500

    Removed import from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { app, BrowserWindow } from "electron";
-import VCDLexer from "./lexer";
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
