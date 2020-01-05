c8000638478c958e620b0f4398d3c92e84de08ee
Mena Wang <wang.mena@gmail.com>
2020-01-04 16:53:37 -0800

    new file structure

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Layout } from './Layout';
+import { Layout } from './components/Layout';
 
 ReactDOM.render(<Layout />, document.getElementById('root'));
