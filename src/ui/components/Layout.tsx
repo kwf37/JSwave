c884459c6da27941b9bea82171be5e76058a3075
Mena Wang <wang.mena@gmail.com>
2020-01-07 15:46:14 -0800

    Changed Layout to use MUI Grid

--- src/ui/components/Layout.tsx
+++ src/ui/components/Layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Grid from '@material-ui/core/Grid';
 import "./Layout.css";
 
 export interface LayoutProps {
@@ -8,13 +9,16 @@ export interface LayoutProps {
 
 export const Layout: React.FC<LayoutProps> = (props: LayoutProps) => {
     return (
-        <div className="outerContainer">
-            <div className="topRow">{props.children[0]}</div>
-
-            <div className="bottomRow">
-                <div className="leftColumn">{props.children[1]}</div>
-                <div className="rightColumn">{props.children[2]}</div>
-            </div>
-        </div>
+        <Grid container>
+            <Grid item xs={12}>
+                <div>{props.children[0]}</div>
+            </Grid>
+            <Grid item xs={4} style={{ minWidth: '250px' }}>
+                <div>{props.children[1]}</div>
+            </Grid>
+            <Grid item xs={8}>
+                <div>{props.children[2]}</div>
+            </Grid>
+        </Grid>
     );
 };
